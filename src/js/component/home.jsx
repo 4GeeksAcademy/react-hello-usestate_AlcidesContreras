03a7ae24f1352bbcb7b11db8b0319f193741f93e
ALCIDESCONTRERAS <alcidescon@gmail.com>
2025-01-10 08:17:55 +0000

    proyecto terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,107 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
 const Home = () => {
+	const [color, setColor] = useState("")
+	const [purpleOn, setPurpleOn] = useState(false)
+	const [intervalId, setIntervalId] = useState(null)
+
+	const changeColorRed = () => {
+		setColor("red")
+	};
+
+	const changeColorGreen = () => {
+		setColor("green")
+	};
+
+	const changeColorYellow = () => {
+		setColor("yellow")
+	};
+
+	const changeColorPurple = () => {
+		setColor("purple")
+	}
+
+	const alternal = () => {
+		setColor((prevColor) => {
+			if (prevColor === "red") return "yellow"
+			if (prevColor === "yellow") return "green"
+			if (prevColor === "green"){
+				if (purpleOn) {
+					return "purple"
+				}else { return "red" }
+			}
+			return "red"
+		})
+	};
+
+
+	const alternalColors = () => {
+		if (!intervalId) {
+			const id = setInterval(alternal, 1000)
+			setIntervalId(id)
+		}
+	};
+
+	const alternalColorsStop = () => {
+		if (intervalId) {
+			clearInterval(intervalId)
+			setIntervalId(null)
+		}
+	}
+
+	const handlePurple = () => {
+		setPurpleOn(true)
+	};
+
+	const handlePurpleOf = () => {
+		setPurpleOn(false)
+	};
+
+	useEffect(() => {
+		return () => {
+			if (intervalId) {
+				clearInterval(intervalId)
+			}
+		}
+	}, [intervalId]);
+	
+	useEffect(() => {
+		if(intervalId){
+			clearInterval(intervalId)
+			const id = setInterval(alternal,1000)
+			setIntervalId(id)
+		}
+	}, [purpleOn]);
+
+
+
+
+	console.log(color);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<main className={`container ${purpleOn ? "container_add" : ""}`}>
+				<div className={`semaforo ${purpleOn ? "semaforo_add" : ""}`}>
+					<span onClick={changeColorRed} className={`circulo red`} id={color === "red" ? "red" : ""}></span>
+					<span onClick={changeColorYellow} className={`circulo circulo_yellow yellow`} id={color === "yellow" ? "yellow" : ""}></span>
+					<span onClick={changeColorGreen} className={`circulo circulo_green`} id={color === "green" ? "green" : ""}></span>
+					{purpleOn ? <span onClick={changeColorPurple} className="circulo circulo_purple" id={color === "purple" ? "purple" : ""}></span> : null}
+				</div>
+			</main>
+
+			<section className="buttons">
+				<button className="button" onClick={alternalColors}>Alternal colors</button>
+				<button className="button" onClick={alternalColorsStop}>Alternal</button>
+				<button className="button" onClick={handlePurple}>Add color</button>
+				<button className="button" onClick={handlePurpleOf}>Remove color</button>
+			</section>
+		</>
+
 	);
 };
 
